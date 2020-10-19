c0f113e31214f9a6e445f1e0d317528bdf23303d
Rina Solomon <rinagrom@gmail.com>
2020-10-19 13:52:36 +0300

    archi files

---
+++ Atemplate.jsx
@@ -0,0 +1,22 @@
+
+
+
+
+class App extends React.Component { 
+    constructor(props) {
+        super(props);
+    }
+
+    render() {
+
+        return (
+           <div>     
+            
+           </div>
+        );
+    
+    }
+
+}
+
+ReactDOM.render(<App/>, document.getElementById('app'));
