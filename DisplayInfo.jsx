62d8b5bfed07bac730c972653f5ed414d93202ff
Rina Solomon <rinagrom@gmail.com>
2020-10-19 21:41:51 +0300

    display info

--- DisplayInfo.jsx
+++ DisplayInfo.jsx
@@ -1,17 +1,59 @@
+class DisplayInfo extends React.Component {
+   
+    
+    constructor(props) {
+        super(props);
+    }
+    render() {
 
+        return (
+           `${this.props.name} works in ${this.props.company}`
+        );
+    
+    }
+}
+
+class DetailsForm extends React.Component  {
+    constructor(props){
+        super(props);
+        this.state = {formData: {
+            name: '',
+            company: ''
+        }}
+    }
+
+   onInputName(event) {
+        this.state.formData.name = event.target.value;
+        this.setState(this.state.formData);
+    }
+
+    onInputCompany(event) {
+        this.state.formData.company = event.target.value;
+        this.setState(this.state.formData);
+    }
+    
+    render() {
+
+        return (
+           <div>     
+             <input onInput={event => this.onInputName(event) }></input>
+             <input onInput={event => this.onInputCompany(event)}></input>
+             <h1><DisplayInfo name={this.state.name} company = {this.state.company}/></h1>
+           </div>
+        );
+    
+    }
+}
 
 
 
 class App extends React.Component { 
-    constructor(props) {
-        super(props);
-    }
 
     render() {
 
         return (
            <div>     
-            
+            <DetailsForm/>            
            </div>
         );
     
